86f09d9155a7b10b0f256c8eba48e6d65b84a86d
gkarm <jlapalo@yahoo.com>
2025-04-29 19:29:31 +0200

    test

---
+++ src/main/java/nl/novi/garageapi/GarageapiApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.garageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/garageapi/GarageapiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.garageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
